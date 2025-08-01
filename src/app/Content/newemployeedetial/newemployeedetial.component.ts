b31a02df889123deb86faf03eeb817cfc37a1b10
Poom <pittayatorn_p@hotmail.com>
2025-08-01 10:58:31 +0700

    new Updatev3

---
+++ src/app/Content/newemployeedetial/newemployeedetial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newemployeedetial',
+  imports: [],
+  templateUrl: './newemployeedetial.component.html',
+  styles: ``
+})
+export class NewemployeedetialComponent {
+  name:string = 'New Employee Detail';
+
+}
